301a8a2916bca625a62300b65390b61668038ef2
igaue-takahhiko <hasewo.2610@gmail.com>
2020-09-12 04:43:35 +0900

    implement routting pages

--- src/index.js
+++ src/index.js
@@ -1,16 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import { ConnectedRouter } from 'connected-react-router';
+import * as History from 'history'
+
 import createStore from './reducks/store/store'
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-export const store = createStore()
+const history = History.createBrowserHistory()
+export const store = createStore(history)
 
 ReactDOM.render(
     <Provider store={store}>
-        <App />
+        <ConnectedRouter history={history}>
+            <App />
+        </ConnectedRouter>
     </Provider>,
     document.getElementById('root')
 );

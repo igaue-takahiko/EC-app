627af7d2dea0e4d154f42973b1625447c8b093de
igaue-takahhiko <hasewo.2610@gmail.com>
2020-09-12 02:19:35 +0900

    create actions.jsx and reducers.jsx

---
+++ src/reducks/users/actions.jsx
@@ -0,0 +1,22 @@
+export const SIN_IN = "SIN_IN"
+export const signInAction = (userState) => {
+    return {
+        payload: {
+            isSignedIn: true,
+            uid: userState.uid,
+            username: userState.username
+        }
+    }
+}
+
+export const SING_OUT = "SIGN_OUT"
+export const signOutAction = () => {
+    return {
+        type: "SIGN_OUT",
+        payload: {
+            isSignOut: false,
+            uid: "",
+            username: "",
+        }
+    }
+}

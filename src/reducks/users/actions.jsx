94559e2ca6b4d30298d19b794170f587ff0a04ec
igaue-takahhiko <hasewo.2610@gmail.com>
2020-09-12 11:36:28 +0900

    learn re-ducks pattern selector

--- src/reducks/users/actions.jsx
+++ src/reducks/users/actions.jsx
@@ -1,7 +1,7 @@
-export const SIN_IN = "SIN_IN"
+export const SIGN_IN = "SIGN_IN"
 export const signInAction = (userState) => {
     return {
-        type: "SIN_IN",
+        type: "SIGN_IN",
         payload: {
             isSignedIn: true,
             uid: userState.uid,
--- src/reducks/users/reducers.jsx
+++ src/reducks/users/reducers.jsx
@@ -3,7 +3,7 @@ import  initialState from '../store/initialState'
 
 export const UserReducer = (state = initialState.users, action) =>{
     switch (action.type) {
-        case Actions.SIN_IN:
+        case Actions.SIGN_IN:
             return {
                 ...state,
                 ...action.payload,

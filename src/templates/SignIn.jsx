c0b6d8247489505e7635fa8d736b92fb470f7fab
igaue-takahhiko <hasewo.2610@gmail.com>
2020-11-18 15:28:16 +0900

    Add to cursor pointer

--- src/templates/SignIn.jsx
+++ src/templates/SignIn.jsx
@@ -38,8 +38,8 @@ const SignIn = () => {
                     onClick={() => dispatch(signIn(email, password))}
                 />
                 <div className="module-spacer--medium" />
-                <p onClick={() => dispatch(push('/signup'))}>アカウントをお持ちでない方はこちら</p>
-                <p onClick={() => dispatch(push('/signin/reset'))}>パスワードを忘れた方はこちら</p>
+                <p className="u-text-small" onClick={() => dispatch(push('/signup'))}>アカウントをお持ちでない方はこちら</p>
+                <p className="u-text-small" onClick={() => dispatch(push('/signin/reset'))}>パスワードを忘れた方はこちら</p>
             </div>
         </div>
     )
